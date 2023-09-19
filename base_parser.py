49050fbde32cf55580949b1e84928f711e2b08e7
Val <valerii.filin@gmail.com>
2023-09-19 14:23:28 +0300

    Base parser refined

--- base_parser.py
+++ base_parser.py
@@ -15,19 +15,38 @@ class BaseParser:
     def __init__(self, result_callback: Callable[[List[Any]], Awaitable[None]]):
         self._result_callback = result_callback
 
+    async def _parse_line(self, line: str):
+        """Parses a single line of data. Should be implemented by subclasses."""
+        raise NotImplementedError
+
     async def parse_stream(
             self,
             pipe: asyncio.StreamReader,
+            encoding: str = "latin1",
+            stop_at_eof: bool = True,
             ) -> None:
-        """Main class method. Parses a stream of data from a pipe. Should be overridden in subclasses."""
-        pass
+        """Main class method. Parses a stream of data from a pipe. If stop_at_eof is True,
+        the method will stop parsing when the pipe is closed. Otherwise, it will wait for
+        new data to arrive.
+        """
+        while True:
+            data = await pipe.readline()
+            if len(data) == 0:  # EOF reached
+                if stop_at_eof:
+                    break
+                else:
+                    await asyncio.sleep(0.1)
+                    continue
 
-    async def parse_file(
+            logger.debug(f"next line to parse: '{data}'")
+            line = data.decode(encoding).rstrip('\n')
+            await self._parse_line(line)
+
+    async def parse_fifo(
             self,
-            file_name: str,
+            fifo_name: str,
             ) -> None:
-        """Helper method. Opens a file and passes it to parse_stream."""
-        with open(file_name, 'rb') as file:
-            reader, _ = await get_stream_reader(file)
-            await self.parse_stream(reader)
-
+        """Reads a stream of data from a named pipe and forwards it to parse_stream.
+        If the named pipe is disconnected from the source -
+        Should be implemented by subclasses."""
+        raise NotImplementedError
