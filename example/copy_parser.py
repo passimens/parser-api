b298217c4a127aa83f18544f4b08abf0192316fb
Val <valerii.filin@gmail.com>
2023-10-19 11:15:09 +0300

    CopyParser: simplest parser example
    Sends all the text data it receives to the callback

---
+++ example/copy_parser.py
@@ -0,0 +1,17 @@
+import logging
+
+from base_parser import BaseParser
+
+logger = logging.getLogger(__name__)
+
+
+class CopyParser(BaseParser):
+    """A simple parser that sends all the data it receives to the callback."""
+
+    async def _parse_line(
+            self,
+            line: str,
+            ) -> None:
+        """Parses a single line of data."""
+        await self._result_callback([line])
+
