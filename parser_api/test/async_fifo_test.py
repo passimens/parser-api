037edced95b6e7c6a3dd2fe4e47f0f57d9015be1
Val <valerii.filin@gmail.com>
2023-11-30 20:01:23 +0200

    AsyncFifo.open(): new parameter allow_eof
    To define the strategy of handling cases when FIFO does not return more data: should we wait for further data whenever it arrives or should we signal EOF.

--- parser_api/test/async_fifo_test.py
+++ parser_api/test/async_fifo_test.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from parser_api.async_fifo import AsyncFifo
 import unittest
 
@@ -74,6 +76,45 @@ class TestAsyncFifoInteractive(unittest.IsolatedAsyncioTestCase):
         with await self.fifo.open("test_fifo") as fifo_reader:
             print("waiting for data via named pipe test_fifo... expect 'gfedcba'")
             self.assertEqual(await fifo_reader.readline(), b"gfedcba\n")
+
+
+class TestAsyncFifoAllowEOF(unittest.IsolatedAsyncioTestCase):
+    """Tests for AsyncFifo with interaction with a named pipe which allows EOF."""
+
+    def setUp(self) -> None:
+        self.fifo = AsyncFifo()
+
+    def tearDown(self) -> None:
+        self.fifo.close()
+
+    async def test_read_until_eof(self):
+        """Tests AsyncFifo _reader within context manager."""
+        self.fifo = AsyncFifo()
+        with await self.fifo.open("test_fifo", allow_eof=True) as fifo_reader:
+            print("waiting for 1 line from named pipe test_fifo...")
+            self.assertNotEqual(await fifo_reader.readline(), b"")
+            print("No more data. So, next attempt to read should return empty line.")
+            self.assertEqual(await fifo_reader.readline(), b"")
+
+
+class TestAsyncFifoDisallowEOF(unittest.IsolatedAsyncioTestCase):
+    """Tests for AsyncFifo with interaction with a named pipe which does not allow EOF."""
+
+    def setUp(self) -> None:
+        self.fifo = AsyncFifo()
+
+    def tearDown(self) -> None:
+        self.fifo.close()
+
+    async def test_read_without_eof(self):
+        """Tests AsyncFifo _reader within context manager."""
+        self.fifo = AsyncFifo()
+        with await self.fifo.open("test_fifo", allow_eof=False) as fifo_reader:
+            print("waiting for 1 line from named pipe test_fifo...")
+            self.assertNotEqual(await fifo_reader.readline(), b"")
+            print("No more data. So, next attempt to read should stuck waiting for data.")
+            with self.assertRaises(asyncio.TimeoutError):
+                await asyncio.wait_for(fifo_reader.readline(), timeout=1)
 
 
 if __name__ == "__main__":
